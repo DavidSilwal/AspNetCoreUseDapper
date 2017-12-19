9574e5b06470a906d19edf951e5c308db9bca05c
毕殿卿 <bdq123@outlook.com>
2017-12-19 23:29:19 +0800

    添加项目文件。

---
+++ Repository.Dapper/Repository.Async.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Dapper
+{
+    public partial class Repository<T> : IRepository<T> where T : class, new()
+    {
+    }
+}
---
+++ Service/Impl/UserService.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Repository;
+
+namespace Service.Impl
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _userRepository;
+        public UserService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+    }
+}
